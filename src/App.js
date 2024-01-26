2a6b2c7a192adf2782567b6426368eafb6f33a7e
saad khan <saad.khan6670@gmail.com>
2024-01-26 23:26:34 +0500

    Todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [inputValue, setInputValue] = useState('')
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleCLick = () =>{
+    setList([...list, inputValue])
+    setInputValue(list);
+  }
+  const handleRemove = (id) =>{
+    const removeItem = list.filter((item) => item.id !== id);
+    setList(removeItem);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo Appliaction</h1>
+      <Form.Control
+        type="text"
+        placeholder="Enter the List"
+        onChange={handleInput}
+        value={inputValue}
+      />
+      <Button onClick={handleCLick} variant="primary">
+        Click
+      </Button>
+
+      <ul>
+        {list.map((item) => (
+          <li key={item}>{item} <button onClick={()=> handleRemove(item.id)}>Delete</button></li>
+        ))}
+      </ul>
     </div>
   );
 }
